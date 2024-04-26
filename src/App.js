8d43a2045e987b5d2949e74eb534702b77629f69
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-04-26 13:03:47 +0100

    feat: Added all routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
+import PlacesToStay from "./pages/PlacesToStay";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+          <Route path="places-to-stay" element={<PlacesToStay />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
