3a279d2a2ac50ac5d2b35136480b71780e6a3070
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-04-26 13:15:35 +0100

    chore: Removed some components from react-scroll that is not needed

--- src/components/NftSection/NftSection.jsx
+++ src/components/NftSection/NftSection.jsx
@@ -1,19 +1,16 @@
-import { Element } from "react-scroll";
 import NftCard from "./NftCard";
 
 export default function NftSection({ nfts }) {
   return (
-    <Element name="nfts">
-      <section className="pt-8 sm:pt-14 pb-8 sm:pb-10 w-full px-[5%]" id="nfts">
-        <h2 className="text-center clamp sm:text-3xl lg:text-4xl font-bold pb-6 sm:pb-8">
-          Inspiration for your next adventure
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center">
-          {nfts.map(nft => (
-            <NftCard key={nft.id} img={nft.img} />
-          ))}
-        </div>
-      </section>
-    </Element>
+    <section className="pt-8 sm:pt-14 pb-10 sm:pb-10 w-full px-[5%]" id="nfts">
+      <h2 className="text-center clamp sm:text-3xl lg:text-4xl font-bold pb-6 sm:pb-8">
+        Inspiration for your next adventure
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center">
+        {nfts.map(nft => (
+          <NftCard key={nft.id} img={nft.img} />
+        ))}
+      </div>
+    </section>
   );
 }
